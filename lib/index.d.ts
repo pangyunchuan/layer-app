34f79d978444116647e276ce1dfca2d0a22158ff
pangyunchuan <897448376@qq.com>
2022-01-12 14:40:54 +0800

    测试

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,14 @@
-/// <reference path="./dts/lib/packages/Models/RequestModel" />
 import RequestModel from "./dts/lib/packages/Models/RequestModel";
+import BaseLoading from "./dts/lib/packages/Loading/BaseLoading";
+import LoadingRequest from "./dts/lib/packages/Request/LoadingRequest";
+import {updateConfig as setLoadingClassConfig} from "./dts/lib/config/requestClassConfig";
+import {updateConfig as setRequestClassConfig} from "./dts/lib/config/loadingClassConfig";
+
 
 export {
-    RequestModel
+    RequestModel,
+    BaseLoading,
+    LoadingRequest,
+    setLoadingClassConfig,
+    setRequestClassConfig
 }
