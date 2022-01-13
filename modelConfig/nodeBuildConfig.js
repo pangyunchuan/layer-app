f0feffdbd1670fb83d6aa4f54342820fb8439ccb
pangyunchuan <897448376@qq.com>
2022-01-13 17:07:52 +0800

    测试

--- modelConfig/nodeBuildConfig.js
+++ modelConfig/nodeBuildConfig.js
@@ -4,8 +4,11 @@ const path = require("path");
 const rootPath = process.cwd()
 
 const useConfigPath = path.resolve(rootPath, 'modelConfig/modelConfig.ts')
+console.log(1, fs.existsSync(useConfigPath), useConfigPath)
+console.log(2, fs.existsSync(path.dirname(useConfigPath)), path.dirname(useConfigPath))
+console.log(3, path.resolve(__dirname, 'modelConfig.ts'))
 if (!fs.existsSync(useConfigPath)) {
-    if(!fs.existsSync(path.dirname(useConfigPath))){
+    if (!fs.existsSync(path.dirname(useConfigPath))) {
         fs.mkdirSync(path.dirname(useConfigPath))
     }
     fs.copyFileSync(path.resolve(__dirname, 'modelConfig.ts'), useConfigPath)
