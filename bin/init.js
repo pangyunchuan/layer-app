93c1356900e16782ea762d315ebbc4393a2066cc
pangyunchuan <897448376@qq.com>
2022-01-17 11:42:45 +0800

    测试

--- bin/init.js
+++ bin/init.js
@@ -1,21 +1,22 @@
+#!/usr/bin/env node
 const fs = require("fs");
 const path = require("path");
 
-const rootPath = process.cwd()
+const rootPath = process.cwd();
 
-const useConfigPath = path.resolve(rootPath, 'modelConfig/index.ts')
+const useConfigPath = path.resolve(rootPath, "modelConfig/index.ts");
 
-console.log('需要文件', useConfigPath)
+console.log("需要文件", useConfigPath);
 
-const useDir = path.dirname(useConfigPath)
+const useDir = path.dirname(useConfigPath);
 
-const packageRoot = path.dirname(__dirname)
+const packageRoot = path.dirname(__dirname);
 
 if (!fs.existsSync(useConfigPath)) {
     if (!fs.existsSync(useDir)) {
-        console.log('创建目录', useDir)
+        console.log("创建目录", useDir)
         fs.mkdirSync(useDir)
     }
-    console.log('原始文件', path.resolve(packageRoot, 'index.ts'))
-    fs.copyFileSync(path.resolve(packageRoot, 'index.ts'), useConfigPath)
+    console.log("原始文件", path.resolve(packageRoot, "index.ts"))
+    fs.copyFileSync(path.resolve(packageRoot, "index.ts"), useConfigPath)
 }
