50edd1be12011431cf2526c1f3bc8a5ca6fd7006
庞云川 <pangyunchuan@bonc.com.cn>
2022-10-19 13:03:54 +0800

    更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,10 @@
-import {defineConfig, loadEnv} from "vite";
+import {defineConfig} from "vite";
 import * as path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-    let env = loadEnv(mode, "");
+export default defineConfig(() => {
     return {
-        // 配置别名
-        resolve: {
-            alias: [
-                {
-                    find: "root/",
-                    replacement: "/"
-                }
-            ],
-            extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
-        },
         build: {
-            rollupOptions: {
-                // 确保外部化处理那些你不想打包进库的依赖
-                external: ['lodash-es', 'axios', 'vue'],
-                output: {
-                    // // // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-                    globals: {
-                        'lodash-es': 'lodash-es',
-                        'axios': 'axios',
-                        'vue': 'vue'
-                    }
-                },
-            },
             lib: {
                 entry: path.resolve(__dirname, 'lib/index.ts'),
                 name: 'MyLib',
